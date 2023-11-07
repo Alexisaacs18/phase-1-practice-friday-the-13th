a103646fc5ff8f573775f9375b6281acc07afd9a
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-07 16:07:44 -0500

    Alex's code

--- src/index.js
+++ src/index.js
@@ -4,44 +4,40 @@ const movieInfoContainer = document.getElementById("movie-info");
 const url = " http://localhost:3000/movies";
 
 function renderMovieInfo(movie) {
-  const detailImage = document.getElementById("detail-image");
-  const title = document.getElementById("title");
-  const yearReleased = document.getElementById("year-released");
-  const description = document.getElementById("description");
-  const watched = document.getElementById("watched");
+    const detailImage = document.getElementById("detail-image");
+    const title = document.getElementById("title");
+    const yearReleased = document.getElementById("year-released");
+    const description = document.getElementById("description");
+    const watched = document.getElementById("watched");
 
-  detailImage.src = movie.image;
-  title.innerText = movie.title;
-  yearReleased.innerText = movie.release_year;
-  description.innerText = movie.description;
-  watched.innerText = movie.watched;
-
-  // const detailImageContent = detailImage.src;
-  // const titleContent = title.textContent;
-  // const yearReleasedContent = yearReleased.textContent;
-  // const descriptionContent = descriptionContent.textContent;
-  // const watchedContent = watched.textContent;
-
-  // detailImageContent.append(movie);
-  // titleContent.append(movie);
-  // yearReleasedContent.append(movie);
-  // descriptionContent.append(movie);
-  // watchedContent.append(movie);
+    detailImage.src = movie.image;
+    title.innerText = movie.title;
+    yearReleased.innerText = movie.release_year;
+    description.innerText = movie.description;
+    watched.innerText = movie.watched;
 }
 
 fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    const movielist = data;
-    // movieImg = movielist[-1].image
-    renderMovieInfo(movielist[0]);
-    movielist.forEach((movie) => {
-      addImg(movie.image);
+    .then((res) => res.json())
+    .then((data) => {
+        const movielist = data;
+        // movieImg = movielist[-1].image
+        renderMovieInfo(movielist[0]);
+        movielist.forEach((movie) => {
+            addImg(movie.image);
+            addEventListener('click', (e) => {
+                for (let i = 0; i < movielist.length; i++) {
+                    if (movielist[i].includes(e.target)) {
+                        renderMovieInfo(movielist[i]);
+                    }
+                }
+            })
+        });
     });
-  });
 
 function addImg(value) {
-  const createImg = document.createElement("img");
-  createImg.src = value;
-  imgContainer.appendChild(createImg);
+    const createImg = document.createElement("img");
+    createImg.src = value;
+    imgContainer.appendChild(createImg);
 }
+
